bedf29eb4179c9a0b89ac7d253defee2f2656d92
asim samuel <asim.samuel1@gmail.com>
2020-11-06 22:55:06 -0500

    Chore: added project data

---
+++ src/Components/data/projects_data.js
@@ -0,0 +1,31 @@
+import CookStars from '../assets/CookStars.png'
+import Shop from '../assets/shop.png'
+import Data from '../assets/datastrr.png'
+const data_projects = [
+  {
+    name: 'CookStars',
+    image: CookStars,
+    github_url: 'https://github.com/CookStars/cookstarsapp',
+
+  },
+
+  {
+    name: 'Fugees Art Shop',
+    image: Shop,
+    github_url: 'https://github.com/graceshopperteamf/grace-shopper-team-f',
+
+  },
+
+  {
+    name: 'iGoYouGoAlgo',
+    image: Data,
+    github_url: 'https://github.com/lordofdorne/iGoYouGoAlgo',
+
+  }
+
+
+
+
+]
+
+export default data_projects;
