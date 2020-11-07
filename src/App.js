e05b9f7273d79c68004249b27808724155b344f8
asim samuel <asim.samuel1@gmail.com>
2020-11-06 19:18:36 -0500

    Chore: fixed routes on APP.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from './Components/Navbar'
 import Sidebar from './Components/Sidebar'
-import { BrowserRouter as Router } from 'react-router-dom'
+import About from './Components/About'
+import Resume from './Components/Resume'
+import Projects from './Components/Projects'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -16,6 +19,17 @@ function App() {
             </div>
             <div className='col-lg-9 app-main-content'>
               <Navbar />
+              <Route path="/">
+                <About />
+              </Route>
+
+              <Route path="/resume">
+                <Resume />
+              </Route>
+
+              <Route path="/projects">
+                <Projects />
+              </Route>
             </div>
           </div>
         </div>
