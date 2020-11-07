0e13a34b0336f353dfdb9dfe789471221827e328
asim samuel <asim.samuel1@gmail.com>
2020-11-06 22:50:25 -0500

    Chore: completed projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data_projects from './data/projects_data'
+import ProjectCard from './ProjectCard';
+
 
 const Projects = () => {
+  const [projects, setProjects] = useState(data_projects)
+
+
   return (
-    <div>
-      <h1>projects</h1>
+    <div className="container projects">
+      <div className="projects__navbar">
+      </div>
+      <div className="row">
+
+        {
+          projects.map(project =>
+            <ProjectCard key={project.name} project={project} />)
+        }
+      </div>
     </div>
   );
-}
+};
 
 export default Projects;
